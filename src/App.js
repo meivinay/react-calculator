e53f481342ee6fe72672bf6ba2e8f218a3435a61
meivinay <tanwarv96@gmail.com>
2021-08-06 22:35:28 +0530

    re-commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ class App extends React.Component{
     stmt:[],
     operator:[],
     operand:[],
+    isPrevNumNAN:undefined,
     result:""
   }
 evaluate=()=>{
+  console.log(this.state.stmt);
+  console.log(this.state.operator);
+  console.log(this.state.operand);
   let b = this.state.operand.pop();
   let a = this.state.operand.pop();
   let op = this.state.operator.pop();
@@ -28,17 +32,30 @@ evaluate=()=>{
   }
 }
 partialReset=()=>{
-  this.setState({stmt:[],operator:[],operand:[]})
+  this.setState({stmt:[],operator:[],operand:[],isPrevNumNAN:true});
 }
 fullReset = ()=>{
-  this.setState({stmt:[],operand:[],operator:[],result:""})
+  this.setState({stmt:[],operand:[],operator:[],result:"",isPrevNumNAN:true});
 }
 handleNumKeys = (value)=>{
-  this.setState({stmt:[...this.state.stmt,value],operand:[...this.state.operand,value]})
+  if(this.state.isPrevNumNAN === false){
+    let lastNum = this.state.operand.pop();
+    let newNum = lastNum*10+value;
+    console.log(newNum +"mul val");
+      this.setState({stmt:[...this.state.stmt,value],operand:[...this.state.operand,newNum],isPrevNumNAN:false})
+  }
+  else{
+    console.log("single val");
+    this.setState({stmt:[...this.state.stmt,value],operand:[...this.state.operand,value],isPrevNumNAN:false})
+  }
 }
 handleOperatorKeys = (op)=>{
-  this.setState({stmt:[...this.state.stmt,op],operator:[...this.state.operator,op]})
-
+  if(this.state.isPrevNumNAN === false){
+    this.setState({stmt:[...this.state.stmt,op],operator:[...this.state.operator,op],isPrevNumNAN:true});
+  }
+  else {
+    alert("Please Enter Valid input"); 
+  }
 }
 handleEvalAction = ()=>{
   let res = this.evaluate();
